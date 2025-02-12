8b1a2d26a90ca17efce062372a313383bfe3ec20
Mariia-Olena Stus <mariia-olena.stus@hys-enterprise.com>
2025-02-12 16:08:05 +0200

    store cast in LS

--- src/app/store/shop.store.ts
+++ src/app/store/shop.store.ts
@@ -2,11 +2,12 @@ import {
   patchState,
   signalStore,
   withComputed,
+  withHooks,
   withMethods,
   withState,
 } from '@ngrx/signals';
-import { initialShopSlice } from './shop.slice';
-import { computed } from '@angular/core';
+import { initialShopSlice, PersistedShopSlice } from './shop.slice';
+import { computed, effect, Signal } from '@angular/core';
 import { buildCartVm, buildProductListVm } from './shop-vm.builder';
 import * as updaters from './shop.updaters';
 
@@ -31,12 +32,31 @@ export const ShopStore = signalStore(
     ),
   })),
   withMethods((store) => ({
-    setSearchWord: (searchWord: string) => patchState(store, updaters.setSearchWord(searchWord)),
-    addToCart: (productId: string) => patchState(store, updaters.addToCart(productId)),
+    setSearchWord: (searchWord: string) =>
+      patchState(store, updaters.setSearchWord(searchWord)),
+    addToCart: (productId: string) =>
+      patchState(store, updaters.addToCart(productId)),
     showCart: () => patchState(store, updaters.showCart()),
     hideCart: () => patchState(store, updaters.hideCart()),
-    incrementQuantity: (productId: string) => patchState(store, updaters.incrementQuantity(productId)),
-    decrementQuantity: (productId: string) => patchState(store, updaters.decrementQuantity(productId)),
+    incrementQuantity: (productId: string) =>
+      patchState(store, updaters.incrementQuantity(productId)),
+    decrementQuantity: (productId: string) =>
+      patchState(store, updaters.decrementQuantity(productId)),
     checkout: () => patchState(store, updaters.checkout()),
+  })),
+  withHooks((store) => ({
+    onInit() {
+      const persisted: Signal<PersistedShopSlice> = computed(() => ({
+        cartQuantities: store.cartQuantities(),
+      }));
+      const valueFromLS: PersistedShopSlice | '' = JSON.parse(
+        localStorage.getItem('shop') || ''
+      );
+      if (valueFromLS) patchState(store, valueFromLS);
+      effect(() => {
+        const persistedValue = persisted();
+        localStorage.setItem('shop', JSON.stringify(persistedValue));
+      });
+    },
   }))
 );
